b93242e1f684afeee96874b16bb36f6ca2972094
Sinus44 <59234792+Sinus44@users.noreply.github.com>
2023-01-21 00:11:53 +0300

    v2
    learning rate add in constructor

--- Perceptron.py
+++ Perceptron.py
@@ -1,10 +1,11 @@
 from random import random
 
 class Perceptron:
-	def __init__(self, inputs):
+	def __init__(self, inputs, learningRate):
 		self.input = [0 for i in range(inputs)]
 		self.w = [random() for i in range(inputs + 1)]
 		self.sum = 0
+		self.learningRate = learningRate
 
 	def exp(self, x):
 		e = 2.718
@@ -28,7 +29,6 @@ class Perceptron:
 	def learn(self, input, out):
 		y = self.predict(input)
 		err = out - y
-		LR = 0.2
 		for i in range(len(self.w)):
-			self.w[i] += (LR * (err)) * self.input[i]
+			self.w[i] += (self.learningRate * (err)) * self.input[i]
 		return err

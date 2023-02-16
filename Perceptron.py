7bcdd7e9b576a18229b97841a79a77cd4fa40842
Sinus44 <59234792+Sinus44@users.noreply.github.com>
2023-02-17 00:26:57 +0300

    v1.0.0

--- Perceptron.py
+++ Perceptron.py
@@ -1,34 +1,38 @@
+# v1.0.0
+
 from random import random
 
 class Perceptron:
+	"""Простой нейрон"""
 	def __init__(self, inputs, learningRate=0.1):
+		"""Конструктор\nПринимает: (int) inputs - кол-во входных нейронов, (float) learningRate - скорость обучения"""
 		self.input = [0 for i in range(inputs)]
 		self.w = [random() for i in range(inputs + 1)]
 		self.sum = 0
 		self.learningRate = learningRate
 
-	def exp(self, x):
-		e = 2.718
-		return e ** x
-
 	def derivative(self, x):
+		"""Производная функции активации\nПринимает: (float) x - число\nВозвращает: (float) - производная функии активации"""
 		return x * (1 - x)
 
 	def activation(self, x):
-		return 1 / (1 + self.exp(-x))
+		"""Функция активации\nПринимает: (float) x - число\nВозвращает: (float) - функия активации"""
+		return 1 / (1 + 2.718 ** -x)
 
 	def predict(self, input):
+		"""Предсказывает выхоное значение исходя из входных данных\nПринимает: (array_float) input - массив входных данных"""
 		self.input = input + [1]
-		sum = 0
-		for i in range(len(self.input)):
-			sum += self.input[i] * self.w[i]
-		self.sum = sum
-		self.out = self.activation(sum)
+		self.out = self.activation(sum([value * self.w[i] for i, value in enumerate(self.input)]))
 		return self.out
 
 	def learn(self, input, out):
+		"""Обучение нейрона с учителем\nПринимает: (array_float) input - массив входных данных, (float) out - ожидаемое выходное значение\nВозвращает: (float) - ошибка между ожидаемым и фактическим результатом"""
 		y = self.predict(input)
 		err = out - y
+		self.learnNoLearer(err)
+    
+	def learnNoLearer(self, err):
+		"""Обучение нейрона с без учителя\nПринимает: (float) err - ошибка нейрона"""
+		if err == 0: return
 		for i in range(len(self.w)):
 			self.w[i] += (self.learningRate * (err)) * self.input[i]
-		return err
